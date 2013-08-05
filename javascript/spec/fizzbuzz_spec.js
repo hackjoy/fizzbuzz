141d9020dee7723602e050e4ec3378497f4683bc
Jack Hoy <jackhoy05@gmail.com>
2013-08-05 17:07:03 +0100

    completed javascript with jasmine tests

---
+++ javascript/spec/fizzbuzz_spec.js
@@ -0,0 +1,5 @@
+describe("FizzBuzz", function() {
+    it("should return the results of the FizzBuzz game from 1 up to the number provided by the user", function() {
+        expect(fizzbuzz(15)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]);
+    });
+});
